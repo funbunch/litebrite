e728741a0186f83a6589b8c1b83ad3cedf44947b
Shannan Bunch <sbunch@HQ-ML-N1WTG8WL.local>
2020-01-10 15:38:09 -0800

    Updates

---
+++ precache-manifest.13d36f59831617d0dcffc314f8e10999.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5858b0ea0e08a66add5addc2a3df18",
+    "url": "/litebrite/index.html"
+  },
+  {
+    "revision": "e08df19cf5621818750b",
+    "url": "/litebrite/static/css/main.de5eb355.chunk.css"
+  },
+  {
+    "revision": "069c020282419a31fdc6",
+    "url": "/litebrite/static/js/2.b18ba848.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/litebrite/static/js/2.b18ba848.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e08df19cf5621818750b",
+    "url": "/litebrite/static/js/main.ee25156d.chunk.js"
+  },
+  {
+    "revision": "4e677f57799d28772ce2",
+    "url": "/litebrite/static/js/runtime-main.03df8f6a.js"
+  }
+]);
