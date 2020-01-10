24f5effa8947704ab735dfdfbaaadd0778686119
Shannan Bunch <sbunch@HQ-ML-N1WTG8WL.local>
2020-01-10 15:31:12 -0800

    Updates

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import LiteBrite from "./components/LiteBrite";
-import './App.css';
-
-function App() {
-  return <LiteBrite classic cols={22} rows={19} />;
-}
-
-export default App;
