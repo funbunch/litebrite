53cfb14a3b4653a3cf884bc493be6cbc558ff9ac
Shannan Bunch <sbunch@HQ-ML-N1WTG8WL.local>
2020-01-10 15:25:34 -0800

    Updates

---
+++ build/precache-manifest.1f8f1cbb4ca71da413f65ca55841f2cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80018bca912a0d9c20474e90aee4d41d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e08df19cf5621818750b",
+    "url": "/static/css/main.de5eb355.chunk.css"
+  },
+  {
+    "revision": "069c020282419a31fdc6",
+    "url": "/static/js/2.b18ba848.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b18ba848.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e08df19cf5621818750b",
+    "url": "/static/js/main.ee25156d.chunk.js"
+  },
+  {
+    "revision": "31848d7f6537b5126587",
+    "url": "/static/js/runtime-main.e46c8096.js"
+  }
+]);
---
+++ src/components/LiteBrite.js
@@ -0,0 +1,102 @@
+import React, { Component } from "react";
+import Peg from "./Peg";
+import "../styles/litebrite.scss"
+
+const colors = ["red", "orange", "blue", "crazy"];
+
+export default class LiteBrite extends Component {
+  state = {
+    activeLights: {},
+    selectedColor: "orange"
+  };
+
+  render() {
+    console.log(this.props);
+    return (
+      <div
+        className={`lite-brite ${
+          this.props.classic ? " lite-brite--classic" : ""
+        }`}
+      >
+        <div className="lite-brite__lights">
+          {Array.from(new Array(this.props.rows)).map((_, x) => (
+            <div className="row" id={`row-${x}`} key={`row-${x}`}>
+              {Array.from(
+                new Array(
+                  this.props.classic
+                    ? x % 2 !== 0
+                      ? this.props.cols - 1
+                      : this.props.cols
+                    : this.props.cols
+                )
+              ).map((_, y) => {
+                const pegState = this.state.activeLights[`${x},${y}`];
+                return (
+                  <div
+                    className="column"
+                    id={`cols-${x}-${y}`}
+                    key={`cols-${x}-${y}`}
+                  >
+                    <Peg
+                      onClick={() => {
+                        const updatedLights = { ...this.state.activeLights };
+                        const coordinateString = `${x},${y}`;
+                        const peg = updatedLights[coordinateString] || {};
+
+                        peg.active = !peg.active;
+                        peg.color = this.state.selectedColor;
+
+                        updatedLights[coordinateString] = peg;
+
+                        this.setState({
+                          activeLights: updatedLights
+                        });
+                      }}
+                      color={
+                        pegState ? pegState.color : this.state.selectedColor
+                      }
+                      isActive={pegState ? pegState.active : false}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          ))}
+        </div>
+        <div className="light-brite__colors">
+          {colors.map(color => (
+            <button
+              className={`color-picker color-picker--${color} ${
+                color === this.state.selectedColor
+                  ? " color-picker--active"
+                  : ""
+              }`}
+              onClick={() => {
+                /** setState of selectedColor to 'blue' **/
+                this.setState({
+                  selectedColor: color
+                });
+              }}
+            >
+              {color}
+            </button>
+          ))}
+
+          <button
+            onClick={() => {
+              const shouldClear = window.confirm("You sure you want to clear?");
+              /** setState of selectedColor to 'blue' **/
+              if (shouldClear) {
+                this.setState({
+                  activeLights: {}
+                });
+              }
+            }}
+          >
+            Clear
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
