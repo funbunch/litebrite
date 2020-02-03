edefd583e987dc000a2c0aa4608a037af9908181
Shannan Bunch <shannan_bunch@hotmail.com>
2020-02-03 12:42:23 -0800

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import LiteBrite from "./components/LiteBrite";
 import './App.css';
 
 function App() {
-  return <LiteBrite classic cols={22} rows={19} />;
+  // return 
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <LiteBrite classic cols={22} rows={19} />
+        </Route>
+
+        <Route exact path='/sheet/:data' render={(props) => {
+          const activeLights = JSON.parse(atob(props.match.params.data));
+          console.log(activeLights)
+          return (
+            <LiteBrite activeLights={activeLights} classic cols={22} rows={19} />
+          )
+        }} />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
--- src/components/LiteBrite.js
+++ src/components/LiteBrite.js
@@ -1,102 +1,111 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import Peg from "./Peg";
 import "../styles/litebrite.scss"
+import { useHistory } from 'react-router-dom'
 
 const colors = ["red", "orange", "blue", "yellow", "lime", "hotpink"];
 
-export default class LiteBrite extends Component {
-  state = {
-    activeLights: {},
-    selectedColor: "orange"
-  };
+export default (props) => {
 
-  render() {
-    console.log(this.props);
-    return (
-      <div
-        className={`lite-brite ${
-          this.props.classic ? " lite-brite--classic" : ""
-        }`}
-      >
-        <div className="lite-brite__lights">
-          {Array.from(new Array(this.props.rows)).map((_, x) => (
-            <div className="row" id={`row-${x}`} key={`row-${x}`}>
-              {Array.from(
-                new Array(
-                  this.props.classic
-                    ? x % 2 !== 0
-                      ? this.props.cols - 1
-                      : this.props.cols
-                    : this.props.cols
-                )
-              ).map((_, y) => {
-                const pegState = this.state.activeLights[`${x},${y}`];
-                return (
-                  <div
-                    className="column"
-                    id={`cols-${x}-${y}`}
-                    key={`cols-${x}-${y}`}
-                  >
-                    <Peg
-                      onClick={() => {
-                        const updatedLights = { ...this.state.activeLights };
-                        const coordinateString = `${x},${y}`;
-                        const peg = updatedLights[coordinateString] || {};
+  const [activeLights, setActiveLights] = useState({})
+  const [selectedColor, setSelectedColor] = useState('orange')
 
-                        peg.active = !peg.active;
-                        peg.color = this.state.selectedColor;
+  console.log('activeLights', activeLights)
+  const history = useHistory()
 
-                        updatedLights[coordinateString] = peg;
+  useEffect(() => {
+    setActiveLights(props.activeLights || {})
+  }, [props.activeLights])
 
-                        this.setState({
-                          activeLights: updatedLights
-                        });
-                      }}
-                      color={
-                        pegState ? pegState.color : this.state.selectedColor
+  return (
+    <div
+      className={`lite-brite ${
+        props.classic ? " lite-brite--classic" : ""
+      }`}
+    >
+      <div className="lite-brite__lights">
+        {Array.from(new Array(props.rows)).map((_, x) => (
+          <div className="row" id={`row-${x}`} key={`row-${x}`}>
+            {Array.from(
+              new Array(
+                props.classic
+                  ? x % 2 !== 0
+                    ? props.cols - 1
+                    : props.cols
+                  : props.cols
+              )
+            ).map((_, y) => {
+              const pegState = activeLights[`${x},${y}`];
+              return (
+                <div
+                  className="column"
+                  id={`cols-${x}-${y}`}
+                  key={`cols-${x}-${y}`}
+                >
+                  <Peg
+                    onClick={() => {
+                      const updatedLights = { ...activeLights };
+                      const coordinateString = `${x},${y}`;
+
+                      if (updatedLights[coordinateString]) delete updatedLights[coordinateString]
+                      else {
+                        updatedLights[coordinateString] = {
+                          active: true,
+                          color: selectedColor
+                        }
                       }
-                      isActive={pegState ? pegState.active : false}
-                    />
-                  </div>
-                );
-              })}
-            </div>
-          ))}
-        </div>
-        <div className="light-brite__colors">
-          {colors.map(color => (
-            <button
-              className={`color-picker color-picker--${color} ${
-                color === this.state.selectedColor
-                  ? " color-picker--active"
-                  : ""
-              }`}
-              onClick={() => {
-                /** setState of selectedColor to 'blue' **/
-                this.setState({
-                  selectedColor: color
-                });
-              }}
-            >
-              {color}
-            </button>
-          ))}
 
+                      setActiveLights(updatedLights)
+
+                      // console.log({
+                      //   updatedLights,
+                      //   string: btoa(JSON.stringify(updatedLights))
+                      // })
+                      // setActiveLights(updatedLights)
+                      const encodedSheet = btoa(JSON.stringify(updatedLights))
+                      history.push(`/sheet/${encodedSheet}`)
+
+                    }}
+                    color={
+                      pegState ? pegState.color : selectedColor
+                    }
+                    isActive={pegState ? pegState.active : false}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        ))}
+      </div>
+      <div className="light-brite__colors">
+        {colors.map(color => (
           <button
+            className={`color-picker color-picker--${color} ${
+              color === selectedColor
+                ? " color-picker--active"
+                : ""
+            }`}
             onClick={() => {
-              const shouldClear = window.confirm("You sure you want to clear?");
               /** setState of selectedColor to 'blue' **/
-              if (shouldClear) {
-                this.setState({
-                  activeLights: {}
-                });
-              }
+              setSelectedColor(color)
             }}
           >
-            Clear
+            {color}
           </button>
-        </div>
+        ))}
+
+        <button
+          onClick={() => {
+            const shouldClear = window.confirm("You sure you want to clear?");
+            /** setState of selectedColor to 'blue' **/
+            if (shouldClear) {
+              history.push('/')
+            }
+          }}
+        >
+          Clear
+        </button>
       </div>
-    );
-  }
+    </div>
+  )
 }
