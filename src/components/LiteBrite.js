1ba0a2b5c5fd9432d6b37ebcf5923fe79c42f9c7
Shannan Bunch <shannan.bunch@tvgla.com>
2020-08-24 15:47:47 -0700

    fixed linting

--- src/components/LiteBrite.js
+++ src/components/LiteBrite.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from "react";
+import React, {useState, useEffect } from "react";
 import Peg from "./Peg";
 import "../styles/litebrite.scss"
 import { useHistory } from 'react-router-dom'
